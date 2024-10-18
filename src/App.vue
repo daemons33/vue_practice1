e93198d27060a156d129a0558bcbc8080726d33d
Marcelo Ovando <equalizer7430@gmail.com>
2024-10-18 17:35:24 -0400

    nuevo proyecto de vuejs

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1>HOLA MUNDOOOO!!!</h1>
+  <p>{{ contador }}</p>
+  <button @click="incrementar">incrementar</button>
+  <button @click="decrementar">decrementar</button>
+</template>
+
+<script setup>
+  import {ref} from 'vue';
+  const contador = ref(0);
+  const incrementar = () =>{
+    contador.value ++;
+  }
+  const decrementar = ()=>{
+    contador.value --
+  }
+</script>
+
+<style scoped>
+
+</style>
