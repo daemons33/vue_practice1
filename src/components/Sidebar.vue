26f02f4dffe2513ff790ffe05a49fcdf0623a455
Marcelo Ovando <equalizer7430@gmail.com>
2024-10-30 16:46:05 -0400

    empezamos en el 8

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="sidebar">
         <h2>Bienvenido</h2>
-        <h3>Nombre: {{ nombre }}</h3>
-        <h3>Email: {{ email }}</h3>
+        <h3>Nombre: {{ registrarStore.nombre }}</h3>
+        <h3>Email: {{ registrarStore.email }}</h3>
         <li><router-link to="/contador">Contador</router-link></li>
         <li><router-link to="/lista-de-tareas">Lista de Tareas</router-link></li>
         <li><router-link to="/registro">Registrar</router-link></li>
@@ -13,15 +13,7 @@
     import {ref, watch} from 'vue';
 
     const registrarStore = useRegistrarStore();
-    const nombre = ref(registrarStore.nombre.value);
-    const email = ref(registrarStore.email.value);
 
-    watch(() => registrarStore.nombre, (newValue)=>{
-        nombre.value= newValue;
-    });
-    watch(() => registrarStore.email, (newValue)=>{
-        email.value= newValue;
-    });
 </script>
 <style scoped>
     .sidebar{
